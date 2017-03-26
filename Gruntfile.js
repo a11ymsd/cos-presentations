2c91aeb8cae6478a66b3721aebc4769474830b0b
Eric Eggert <mail@yatil.net>
2017-03-26 15:23:36 +0200

    Fonts and color update

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
 					'styles/screen.css' : 'styles/screen.css'
 				},
 				options: {
-					banner: '/**\n * <%= pkg.description %>\n * <%= pkg.name %> v<%= pkg.version %>, <%= pkg.homepage %>\n * Copyright © 2010–<%= grunt.template.today("yyyy") %> Vadim Makeev, http://pepelsbey.net\n * Licensed under MIT license: github.com/shower/shower/wiki/MIT-License\n */\n'
+					banner: '/**\n * <%= pkg.description %>\n * <%= pkg.name %> v<%= pkg.version %>, <%= pkg.homepage %>\n * Copyright © 2010–<%= grunt.template.today("yyyy") %> Eric Eggert, https://yatil.net, based on work from Vadim Makeev, http://pepelsbey.net\n * Licensed under MIT license: github.com/shower/shower/wiki/MIT-License\n */\n'
 				}
 			}
 		},
